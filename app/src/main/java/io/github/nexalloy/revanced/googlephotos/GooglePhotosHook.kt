02e793dc2324055276941148428dcb002cb3a7a0
ChsBuffer <33744752+chsbuffer@users.noreply.github.com>
2026-04-21 17:18:29 +0800

    feat(Google Photos): Add Enable DCIM folders backup control patch

--- app/src/main/java/io/github/nexalloy/revanced/googlephotos/GooglePhotosHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/googlephotos/GooglePhotosHook.kt
@@ -1,5 +1,9 @@
 package io.github.nexalloy.revanced.googlephotos
 
+import io.github.nexalloy.revanced.googlephotos.misc.backup.EnableDCIMFoldersBackupControl
 import io.github.nexalloy.revanced.googlephotos.misc.features.SpoofFeaturesPatch
 
-val GooglePhotosPatches = arrayOf(SpoofFeaturesPatch)
+val GooglePhotosPatches = arrayOf(
+    SpoofFeaturesPatch,
+    EnableDCIMFoldersBackupControl,
+)
