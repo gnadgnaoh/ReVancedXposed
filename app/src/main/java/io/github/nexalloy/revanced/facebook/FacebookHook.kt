41353a29e79a69ae988b37f28b0fac7c0dee6412
gnadgnaoh <117189041+gnadgnaoh@users.noreply.github.com>
2026-04-30 15:28:08 +0700

    Create FacebookHook.kt

---
+++ app/src/main/java/io/github/nexalloy/revanced/facebook/FacebookHook.kt
@@ -0,0 +1,7 @@
+package io.github.nexalloy.revanced.facebook
+
+import io.github.nexalloy.revanced.facebook.ads.HideAds
+import io.github.nexalloy.revanced.facebook.ads.HideGameAds
+import io.github.nexalloy.revanced.facebook.ads.HideProfileReelsAds
+
+val FacebookPatches = arrayOf(HideAds, HideGameAds, HideProfileReelsAds)
