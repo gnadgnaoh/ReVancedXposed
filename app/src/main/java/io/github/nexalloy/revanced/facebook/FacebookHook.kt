cc049422ed22ad08a58f75107ac77d3ecc6a55e7
XES SEX <117189041+gnadgnaoh@users.noreply.github.com>
2026-09-09 05:49:25 +0700

    Update FacebookHook.kt

--- app/src/main/java/io/github/nexalloy/revanced/facebook/FacebookHook.kt
+++ app/src/main/java/io/github/nexalloy/revanced/facebook/FacebookHook.kt
@@ -1,13 +1,9 @@
 package io.github.nexalloy.revanced.facebook
 
-import io.github.nexalloy.revanced.facebook.ad.BlockFacebookAdRequests
-import io.github.nexalloy.revanced.facebook.ad.HideFacebookAdComponents
 import io.github.nexalloy.revanced.facebook.ad.HideFacebookAds
 import io.github.nexalloy.revanced.facebook.ad.HideProfileTimelineAds
 
 val FacebookPatches = arrayOf(
     HideFacebookAds,
-    HideFacebookAdComponents,
     HideProfileTimelineAds,
-    BlockFacebookAdRequests,
 )
